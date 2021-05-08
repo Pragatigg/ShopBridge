6252d07cf57975fc2cda342c0dee6e248dceaecf
Pragati Garud <pragatigarud94@gmail.com>
2021-05-08 17:06:03 +0530

    Header changes

--- src/app.js
+++ src/app.js
@@ -7,13 +7,17 @@ import {
 
 import List from 'containers/Product/List';
 import Details from 'containers/Product/Details';
+import shopBridge from 'assets/shop-bridge.png';
 
 const { Header, Content, Footer } = Layout;
 
 const App = () => (
   <Layout>
     <Header>
-      <h1>ShopBridge</h1>
+      <h1>
+        <img src={shopBridge} alt="logo" />
+        <span className="ml-2"> ShopBridge </span>
+      </h1>
     </Header>
     <Content className="site-layout">
       <div className="site-layout-background layout-conatiner">
