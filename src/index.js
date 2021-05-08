9ed9db7bbab339eec3f233ff19aec0935cfff602
Pragati Garud <pragatigarud94@gmail.com>
2021-05-08 16:46:37 +0530

    Layout changes

--- src/index.js
+++ src/index.js
@@ -1,52 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Layout } from 'antd';
 import { Provider } from 'react-redux';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
 
-
-import List from 'containers/Product/List';
-import Details from 'containers/Product/Details';
+import App from 'app';
 import store from 'redux/store';
 import reportWebVitals from 'reportWebVitals';
 import 'antd/dist/antd.css';
 import 'index.scss';
 
-const { Header, Content, Footer } = Layout;
 const ROOT = document.getElementById('root');
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Layout>
-        <Header>
-          <h1>ShopBridge</h1>
-        </Header>
-        <Content className="site-layout">
-          <div className="site-layout-background layout-conatiner">
-            <Router>
-              <Switch>
-                <Route path="/" exact>
-                  <List />
-                </Route>
-                <Route path="/new">
-                  <Details />
-                </Route>
-                <Route path="/products/:id">
-                  <Details />
-                </Route>
-              </Switch>
-            </Router>
-          </div>
-        </Content>
-        <Footer>
-          Created by <b>Pragati Garud</b>
-        </Footer>
-      </Layout>
+      <App />
     </Provider>
   </React.StrictMode>,
   ROOT
